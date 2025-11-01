c30356502362ccf9ad88192ff351d1e48c3d70b7
aya-136 <ayazeyad136@gmail.com>
2025-11-01 22:15:08 +0300

    initial commit

---
+++ src/main/java/com/example/querynest/QuerynestApplication.java
@@ -0,0 +1,13 @@
+package com.example.querynest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerynestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerynestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/querynest/QuerynestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querynest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerynestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
